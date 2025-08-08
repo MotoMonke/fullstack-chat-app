bbc6a4928521fd2b7f59dbfcecb3b9568717777a
Daniil <developmentweb15@gmail.com>
2025-08-08 19:58:54 +0300

    Added users sidebar and fixed small error in backend

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,8 +3,8 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 const router = express.Router();
 
-router.get('/user',protectRoute,getUsersForSidebar);
-router.get('/:id',protectRoute,getMessages);
+router.get('/user',protectRoute,getMessages);
+router.get('/:id',protectRoute,getUsersForSidebar);
 
 router.post('/send/:id',protectRoute,sendMessage);
 
