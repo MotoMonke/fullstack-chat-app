fb4fc8aeba4b74b2fe07aa10fb9fd46d52035027
Daniil <developmentweb15@gmail.com>
2025-08-10 17:02:54 +0300

    Configured for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,9 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import {connectDB} from './lib/db.js';
 import {app,server} from './lib/socket.js'
+import path from 'path';
 dotenv.config();
-
+const __dirname = path.resolve(); 
 app.use(express.json({limit:'10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
@@ -19,7 +20,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT;
-
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+    app.get(/(.*)/,(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend','dist','index.html'));
+    });
+}
 const startServer = async () => {
     try {
         await connectDB();
